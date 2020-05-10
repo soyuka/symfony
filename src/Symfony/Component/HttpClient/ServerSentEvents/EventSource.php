7d069d8449b7aa6f365d982f5c040bb6a13185c1
soyuka <soyuka@users.noreply.github.com>
2020-05-10 15:07:30 +0200

    temp

--- src/Symfony/Component/HttpClient/ServerSentEvents/EventSource.php
+++ src/Symfony/Component/HttpClient/ServerSentEvents/EventSource.php
@@ -3,22 +3,75 @@
 namespace Symfony\Component\HttpClient\ServerSentEvents;
 
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\PluggableHttpClient;
+use Symfony\Component\HttpClient\Response\PluggableResponse;
 use Symfony\Component\HttpClient\HttpClientTrait;
+use Symfony\Contracts\HttpClient\ChunkInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 final class EventSource implements HttpClientInterface
 {
-    use HttpClientTrait;
-
     private $client;
-    private $lastEventId = null;
-    private $reconnectionTime = 10;
+    private $lastEventId;
+    private $reconnectionTime;
 
     public function __construct(HttpClientInterface $client = null, array $defaultOptions = [])
     {
-        $this->client = $client ?? HttpClient::create($defaultOptions);
+        $client = $client ?? HttpClient::create();
+        $self = $this;
+        $this->client = new PluggableHttpClient($client, function (HttpClientInterface $client, string $method, string $url, array $options) use ($self) {
+          $bomRemoved = false;
+          $buffer = '';
+          $content = '';
+
+          return new PluggableResponse($client, $method, $url, $options, function (ChunkInterface $chunk) use ($content, $buffer, $bomRemoved, $self) {
+              if ($chunk->isTimeout() || $chunk->isFirst() || $chunk->isLast() || !$chunk->getContent()) {
+                  yield $chunk;
+                  return;
+              }
+
+              $content .= $chunk->getContent();
+              $lines = preg_split("/\n|\r|\r\n/", $content);
+              $buffer = '';
+
+              foreach ($lines as $line) {
+                  // end-of-line, parse full buffer
+                  if ('' === $line) {
+                      if (!$bomRemoved) {
+                          // replace BOM if it exists
+                          $buffer = preg_replace('/\x{FEFF}/u', '', $buffer);
+                          $bomRemoved = true;
+                      }
+
+                      if (!$buffer || 0 === strpos($buffer, ':')) {
+                          $buffer = '';
+                          continue;
+                      }
+
+                      $message = MessageEvent::parse($buffer);
+                      $buffer = '';
+
+                      if (null !== $message->getId()) {
+                          $self->lastEventId = $message->getId();
+                      }
+
+                      if (null !== $message->getRetry()) {
+                          $self->reconnectionTime = $message->retry;
+                      }
+
+                      yield $message;
+                      continue;
+                  }
+
+                  $buffer .= $line."\n";
+              }
+
+              // keep the current non-processed content
+              $content = $buffer;
+          });
+        });
     }
 
     public function stream($responses, float $timeout = null): ResponseStreamInterface
@@ -38,75 +91,5 @@ final class EventSource implements HttpClientInterface
         }
 
         return $this->client->request($method, $url, $options);
-    }
-
-    public function messages($response): \Iterator
-    {
-        while (true !== $response->getInfo('canceled')) {
-            $bomRemoved = false;
-            $buffer = '';
-            $content = '';
-            foreach ($this->client->stream($response, $this->reconnectionTime) as $chunk) {
-                if ($chunk->isTimeout()) {
-                    yield $chunk;
-                    break;
-                }
-
-                if ($chunk->isFirst()) {
-                    yield $chunk;
-                    continue;
-                }
-
-                // connection closed
-                if ($chunk->isLast() || !$chunk->getContent()) {
-                    yield $chunk;
-
-                    return null;
-                }
-
-                $content .= $chunk->getContent();
-                $lines = preg_split("/\n|\r|\r\n/", $content);
-                $buffer = '';
-
-                foreach ($lines as $line) {
-                    // end-of-line, parse full buffer
-                    if ('' === $line) {
-                        if (!$bomRemoved) {
-                            // replace BOM if it exists
-                            $buffer = preg_replace('/\x{FEFF}/u', '', $buffer);
-                            $bomRemoved = true;
-                        }
-
-                        if (!$buffer || 0 === strpos($buffer, ':')) {
-                            $buffer = '';
-                            continue;
-                        }
-
-                        $message = MessageEvent::parse($buffer);
-                        $buffer = '';
-
-                        if (null !== $message->getId()) {
-                            $this->lastEventId = $message->getId();
-                        }
-
-                        if (null !== $message->getRetry()) {
-                            $this->reconnectionTime = $message->retry;
-                        }
-
-                        yield $message;
-
-                        continue;
-                    }
-
-                    $buffer .= $line."\n";
-                }
-
-                // keep the current non-processed content
-                $content = $buffer;
-            }
-
-						usleep($this->reconnectionTime * 1000);
-            $response = $this->request($response->getInfo('http_method'), $response->getInfo('url'));
-        }
     }
 }
