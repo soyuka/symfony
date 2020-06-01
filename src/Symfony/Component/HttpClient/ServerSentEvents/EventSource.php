2e8c5d8c7c323104a6635db1adcd97a277ebc052
soyuka <soyuka@users.noreply.github.com>
2020-06-01 21:28:25 +0200

    bump

--- src/Symfony/Component/HttpClient/ServerSentEvents/EventSource.php
+++ src/Symfony/Component/HttpClient/ServerSentEvents/EventSource.php
@@ -2,102 +2,97 @@
 
 namespace Symfony\Component\HttpClient\ServerSentEvents;
 
-use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\HttpClient\HttpClientTrait;
-use Symfony\Component\HttpClient\PluggableHttpClient;
-use Symfony\Component\HttpClient\Response\PluggableContext;
-use Symfony\Component\HttpClient\Response\PluggableResponse;
+use Symfony\Component\HttpClient\AsyncDecoratorTrait;
+use Symfony\Component\HttpClient\Response\AsyncContext;
+use Symfony\Component\HttpClient\Response\AsyncResponse;
 use Symfony\Contracts\HttpClient\ChunkInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\ResponseInterface;
-use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 final class EventSource implements HttpClientInterface
 {
-    use HttpClientTrait;
+    use AsyncDecoratorTrait;
 
-    private $client;
+    private $reconnectionTime;
 
-    public function __construct(HttpClientInterface $client = null, array $defaultOptions = [])
-    {
-        if (null === $client) {
-            $client = HttpClient::create($defaultOptions);
-        }
-
-        $this->client = new PluggableHttpClient($client, [$this, 'pluggableResponseFactory']);
+    public function __construct(HttpClientInterface $client = null, $reconnectionTime = 2) {
+        $this->client = $client ?: HttpClient::create();
+        $this->reconnectionTime = 2;
     }
 
-    public function pluggableResponseFactory(HttpClientInterface $client, string $method, string $url, array $options): PluggableResponse
+    public function request(string $method, string $url, array $options = []): ResponseInterface
     {
-        $content = '';
+      $content = '';
+      $buffer = '';
+      $bomRemoved = false;
+      $lastEventId = null;
+      $reconnectionTime = $this->reconnectionTime;
+
+      $options['headers'] = array_merge($options['headers'] ?? [], $this->getHeaders());
+      $options['timeout'] = $reconnectionTime;
+
+      return new AsyncResponse($this->client, $method, $url, $options, static function (ChunkInterface $chunk, AsyncContext $context) use ($content, $buffer, $bomRemoved, $lastEventId, $reconnectionTime) {
+        if ($chunk->isTimeout()) {
+          // if (!$context->getInfo('canceled')) {
+          //   $context->replaceRequest($context->getInfo('http_method'), $context->getInfo('url'), ['headers' => []]);
+          // }
+          yield $chunk;
+          return;
+        }
+
+        if ($chunk->isFirst()) {
+          yield $chunk;
+          return;
+        }
+
+        // connection closed
+        if ($chunk->isLast()) {
+          yield $chunk;
+          return;
+        }
+
+        $content .= $chunk->getContent();
+        $lines = preg_split("/\n|\r|\r\n/", $content);
         $buffer = '';
-        $bomRemoved = false;
-        $lastEventId = null;
-        $reconnectionTime = 10;
 
-        return new PluggableResponse($client, $method, $url, $options, function (ChunkInterface $chunk, PluggableContext $context) use ($content, $buffer, $bomRemoved, $lastEventId, $reconnectionTime) {
-            if ($chunk->isTimeout()) {
-                yield $chunk;
-
-                return;
+        foreach ($lines as $line) {
+          // end-of-line, parse full buffer
+          if ('' === $line) {
+            if (!$bomRemoved) {
+              // replace BOM if it exists
+              if (false !== $pos = strpos($buffer, "\u{FEFF}"));
+              $buffer = substr($buffer, $pos, 1);
+              $bomRemoved = true;
             }
 
-            if ($chunk->isFirst()) {
-                yield $chunk;
-
-                return;
+            if ('' === $buffer || 0 === strpos($buffer, ':')) {
+              $buffer = '';
+              continue;
             }
 
-            // connection closed
-            if ($chunk->isLast() || !$chunk->getContent()) {
-                yield $chunk;
-                if (false === $context->getInfo('canceled')) {
-                    $context->replaceRequest($context->getInfo('http_method'), $context->getInfo('url'), ['headers' => $this->getHeaders()]);
-                }
-
-                return;
-            }
-
-            $content .= $chunk->getContent();
-            $lines = preg_split("/\n|\r|\r\n/", $content);
+            $message = MessageEvent::parse($buffer);
             $buffer = '';
 
-            foreach ($lines as $line) {
-                // end-of-line, parse full buffer
-                if ('' === $line) {
-                    if (!$bomRemoved) {
-                        // replace BOM if it exists
-                        $buffer = preg_replace('/^\x{FEFF}/u', '', $buffer);
-                        $bomRemoved = true;
-                    }
-
-                    if ('' === $buffer || 0 === strpos($buffer, ':')) {
-                        $buffer = '';
-                        continue;
-                    }
-
-                    $message = MessageEvent::parse($buffer);
-                    $buffer = '';
-
-                    if (null !== $message->getId()) {
-                        $lastEventId = $message->getId();
-                    }
-
-                    if (0 !== $message->getRetry()) {
-                        $reconnectionTime = $message->retry;
-                    }
-
-                    yield $message;
-
-                    continue;
-                }
-
-                $buffer .= $line."\n";
+            if (null !== $message->getId()) {
+              $lastEventId = $message->getId();
             }
 
-            // keep the current non-processed content
-            $content = $buffer;
-        });
+            if (0 !== $message->getRetry()) {
+              $reconnectionTime = $message->retry;
+            }
+
+            yield $message;
+
+            continue;
+          }
+
+          $buffer .= $line."\n";
+        }
+
+        // keep the current non-processed content
+        $content = $buffer;
+      });
     }
 
     private function getHeaders($lastEventId = null): array
@@ -112,17 +107,5 @@ final class EventSource implements HttpClientInterface
         }
 
         return $headers;
-    }
-
-    public function stream($responses, float $timeout = null): ResponseStreamInterface
-    {
-        return $this->client->stream($responses, $timeout);
-    }
-
-    public function request(string $method, string $url, array $options = []): ResponseInterface
-    {
-        $options['headers'] = array_merge($options['headers'] ?? [], $this->getHeaders());
-
-        return $this->client->request($method, $url, $options);
     }
 }
