8c0c8fe6f93145efe10c20cbe8203dda8d514ba3
soyuka <soyuka@users.noreply.github.com>
2025-09-12 23:20:10 +0200

    [ObjectMapper] cached mapper with dumped code

---
+++ src/Symfony/Component/ObjectMapper/Internal/MappingCacheGeneratorInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Symfony\Component\ObjectMapper\Internal;
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @internal
+ */
+interface MappingCacheGeneratorInterface
+{
+    /**
+     * Generates the PHP code for a mapping function between a source and a target class.
+     *
+     * @param class-string $sourceClass
+     * @param class-string $targetClass
+     *
+     * @throws Symfony\Component\ObjectMapper\Exception\MappingException if code generation fails
+     */
+    public function generate(string $sourceClass, string $targetClass): string;
+
+    /**
+     * Writes the generated mapping code to a cache file.
+     */
+    public function write(string $cacheFile, string $code): void;
+}
